9bd4d0b749e89634439aec34220d880743611f52
D_Zucho <danny.nico.dev@outlook.com>
2024-03-06 17:12:57 -0800

    finish initial header styles

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -2,6 +2,8 @@
 import { useMediaQuery } from 'usehooks-ts'
 import { Menu } from './Icons'
 import { Button } from './ui/button'
+import { Sheet, SheetContent, SheetTitle, SheetTrigger } from './ui/sheet'
+import { Separator } from './ui/separator'
 
 const NavLinks = () => {
   const isMobile = useMediaQuery('(max-width: 768px)')
@@ -9,15 +11,31 @@ const NavLinks = () => {
     <nav>
       {!isMobile ? (
         <div className='flex gap-2 text-white'>
-          <Button className='btn-primary hover:bg-primary-hover shadow-lg'>
+          <Button className='btn-primary hover:bg-primary-hover shadow-lg active:scale-95 transition-all'>
             Sign In
           </Button>
-          <Button className='bg-secondary hover:bg-secondary-hover shadow-lg'>
+          <Button className='bg-secondary hover:bg-secondary-hover shadow-lg active:scale-95 transition-all'>
             Join Now
           </Button>
         </div>
       ) : (
-        <Menu />
+        <Sheet>
+          <SheetTrigger>
+            <Menu />
+          </SheetTrigger>
+          <SheetContent className='bg-bg_cream flex flex-col gap-4 text-white'>
+            <SheetTitle className='text-center'>
+              Welcome to useCookbook!
+            </SheetTitle>
+            <Separator className='bg-slate-400 rounded-full mb-4' />
+            <Button className='btn-primary hover:bg-primary-hover shadow-lg active:scale-95 transition-all'>
+              Sign In
+            </Button>
+            <Button className='bg-secondary hover:bg-secondary-hover shadow-lg active:scale-95 transition-all'>
+              Join Now
+            </Button>
+          </SheetContent>
+        </Sheet>
       )}
     </nav>
   )
